fc5448608e7794a84af019c768786a450ba770fd
rogawas <gitecrng@gmail.com>
2020-09-28 10:22:23 +0200

    Screen width manipulation for better window size

--- src/Break_Out_Game.cpp
+++ src/Break_Out_Game.cpp
@@ -8,28 +8,29 @@
 
 #include <iostream>
 #include <SDL.h>
-#include "Vec2D.h"
 
-const int SCREEN_WIDTH = 680;
-const int SCREEN_HEIGHT = 480;
+#include "Color.h"
+#include "Screen.h"
+#include "Line2D.h"
+
+const int SCREEN_WIDTH = 205;
+const int SCREEN_HEIGHT = 288;
+const int MAGNIFICATION = 3;
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int main(int argc, const char * argv[])
+{
+	Screen theScreen;
 
-	if(SDL_Init(SDL_INIT_VIDEO))
-	{
-		std::cout<<"Error SDL_Init Failed"<<std::endl;
-		return 1;
-	}
+	theScreen.Init(SCREEN_WIDTH, SCREEN_HEIGHT, MAGNIFICATION);
 
-	SDL_Window* optrwindow = SDL_CreateWindow("Hello_World", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT,0);
+	Line2D line = {Vec2D(0, 0), Vec2D(SCREEN_WIDTH, SCREEN_HEIGHT)};
 
-    if(optrwindow==nullptr)
-    {
-    	std::cout<<"could not create window, got error:"<<SDL_GetError()<<std::endl;
-    	return 1;
-    }
+	theScreen.Draw(line, Color::White());
+
+	//theScreen.Draw(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, Color::Yellow());
+	theScreen.SwapScreens();
 
 	SDL_Event sdlEvent;
 	bool running = true;
@@ -38,7 +39,7 @@ int main(int argc, char* argv[]) {
 	{
 		while(SDL_PollEvent(&sdlEvent))
 		{
-			switch(sdlEvent.type)
+			switch (sdlEvent.type)
 			{
 			case SDL_QUIT:
 				running = false;
@@ -47,8 +48,5 @@ int main(int argc, char* argv[]) {
 		}
 	}
 
-	SDL_DestroyWindow(optrwindow);
-	SDL_Quit();
-
-	return 0;
+    return 0;
 }
